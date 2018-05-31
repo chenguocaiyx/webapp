c58f5b5dc1d07490b0e6194f0025b8955d145221
chenguocaiyx <1396890578@qq.com>
2018-05-31 19:28:40 +0800

    添加 photoSwiper 插件

--- script/main.js
+++ script/main.js
@@ -33,3 +33,30 @@ $(document).ready(function () {
        $("#myPanel").panel("open");
    });
 });
+
+// swiper插件
+var swiper = new Swiper('.swiper-container',{
+    direction : 'vertical',
+    followFinger : false,
+    speed:800,
+    mousewheel: true,
+    pagination : {
+        el:'.swiper-pagination',
+    },
+    on:{
+        init:function(swiper){
+            slide=this.slides.eq(0);
+            slide.addClass('ani-slide');
+        },
+        transitionStart: function(){
+            for(i=0;i<this.slides.length;i++){
+                slide=this.slides.eq(i);
+                slide.removeClass('ani-slide');
+            }
+        },
+        transitionEnd: function(){
+            slide=this.slides.eq(this.activeIndex);
+            slide.addClass('ani-slide');
+        },
+    }
+});
